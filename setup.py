3c25c4fde997e22e29dcc3f729de1bfc94167048
Teddy Groves <groves.teddy@gmail.com>
2021-03-18 16:02:55 +0100

    Fix setup bug

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 from setuptools import setup
 
-setup()
+setup(
+    install_requires=[
+        "maud @ git+ssh://git@github.com/biosustain/Maud@master#egg=maud",
+        "arviz>=0.11.2",
+        "plotnine",
+        "matplotlib",
+        "pandas",
+        "numpy",
+        "typer",
+    ]
+)
