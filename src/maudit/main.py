2e602a89b54052361a28e1fe7738d25c454c50da
NicholasCowie <nicow@biosustain.dtu.dk>
2021-03-18 14:54:23 +0100

    chore: updating to incorporte typer

--- src/maudit/main.py
+++ src/maudit/main.py
@@ -1,30 +1,28 @@
 import os
+import typer
 
 from utils import return_dict_of_infd
-
-import pandas as pd
-
+from pathlib import Path
 from maud.io import load_maud_input_from_toml
 
-HERE = os.path.dirname(os.path.abspath(__file__))
-PATHS = {
-    "MAUD_OUTPUT": "../../data/example_maud_outputs/maud_output-methionine_cycle-20210308163837"
-}
+def main(path_to_output_dir: Path):
+    """Run maudit: this is the main entrypoint.
 
-def main():
-    MAUD = os.path.join(HERE, PATHS["MAUD_OUTPUT"], "samples")
-    ui_path = os.path.join(HERE, PATHS["MAUD_OUTPUT"], "user_input")
+    :param path_to_output_dir: Path to a directory that was created by maud to
+    store output files.
+
+    """
+    ui_path = os.path.join(path_to_output_dir, "user_input")
+    samples_dir = os.path.join(path_to_output_dir, "samples")
+    csvs = [os.path.join(samples_dir, f) for f in os.listdir(samples_dir) if f.endswith("csv")]
+    typer.echo(f"Reading data from {path_to_output_dir}")
+    typer.echo(f"Found csv files: {csvs}")
     mi = load_maud_input_from_toml(ui_path)
-    csvs = [
-        os.path.join(HERE, PATHS["MAUD_OUTPUT"], "samples", f)
-        for f in os.listdir(os.path.join(HERE, PATHS["MAUD_OUTPUT"], "samples"))
-        if f.endswith(".csv")
-    ]
     infd_dict = return_dict_of_infd(csvs, mi)
-
     print(infd_dict)
 
     return
 
 if __name__ == "__main__":
-    main()
+    typer.run(main)
+
