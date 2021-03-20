50ea6360ab4a3f3fc80b0c7486776121824e7e76
NicholasCowie <nicow@biosustain.dtu.dk>
2021-03-20 11:40:40 +0100

    feat: include docstring for dict return function

--- src/maudit/utils.py
+++ src/maudit/utils.py
@@ -8,6 +8,10 @@ import pandas as pd
 from maud.analysis import load_infd
 
 def return_dict_of_infd(csvs, mi):
+	"""Return dict of chain with associated infd object.
+	:params csvs: a list of csv file paths
+	:params mi: a MaudInput object
+	"""
 	return {
 		re.split("\.", re.split('-', chain)[-1])[0]: load_infd(chain, mi)
 		for chain in csvs
