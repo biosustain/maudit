bd889e9f299d07706fa6cc534746b9c3ba31d50f
NicholasCowie <nicow@biosustain.dtu.dk>
2021-03-20 12:04:36 +0100

    feat: print var/drawseries given any pandas df with var name

--- src/maudit/plotting.py
+++ src/maudit/plotting.py
@@ -3,13 +3,9 @@
 from plotnine import *
 import pandas as pd
 
-def plot_var_time_series(dict_of_idata):
-    lp_df = pd.DataFrame({
-        chain: idata.sample_stats["lp"].to_series()
-        for chain, idata in dict_of_idata.items()
-    }).droplevel('chain').rename_axis('chain', axis='columns').stack().rename('lp').reset_index()   
+def plot_var_time_series(var_df, var_name):
     p = (
-        ggplot(lp_df, aes(x = 'draw', y = 'lp', color = 'factor(chain)'))
+        ggplot(var_df, aes(x = 'draw', y = var_name, color = 'factor(chain)'))
         +geom_line()
     )
     return p
